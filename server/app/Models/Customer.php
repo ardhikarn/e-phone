9a0f7c2230618c0d2209eb0db29ebf780713e48d
Ardhika Ryzha Nurmawan <ardhikarn@gmail.com>
2021-12-18 21:22:03 +0700

    first commit

---
+++ server/app/Models/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [ 'name', 'email', 'email_verified_at', 'password','remember_token'];
+
+    /**
+     * invoice
+     *
+     * @return void
+     */
+    public function invoices() {
+        return $this->hasMany(Invoice::class);
+    }
+
+}
---
+++ server/app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = ['invoice_id', 'product_id', 'qty', 'price'];
+    
+    public function reviews() {
+        return $this->hasMany(Review::class);
+    }
+}
